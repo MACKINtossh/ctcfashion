a24740a855556c98e9b3ad5d341b0210b0943e74
Jordan Fontaine <jordanmfontaine@gmail.com>
2023-04-30 05:41:50 -0400

    import deps for Customizer page. Completed the custom button and implemented functionality

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { snapshot } from "valtio";
+import state from "../store";
 
-const CustomButton = () => {
+const CustomButton = ({ type, title, customStyles, handleClick }) => {
+  const snap = snapshot(state);
+
+  const generateStyle = (type) => {
+    if (type === "filled") {
+      return {
+        backgroundColor: snap.color,
+        color: "#fff",
+      };
+    }
+  };
+
   return (
-    <div>CustomButton</div>
-  )
-}
+    <button
+      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+      style={generateStyle(type)}
+      onClick={handleClick}
+    >
+      {title}
+    </button>
+  );
+};
 
-export default CustomButton
+export default CustomButton;
